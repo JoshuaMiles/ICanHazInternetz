e96fc88fa1c91d0617d4dc13f1b9c6a544a1c97d
Elliott Moore <elliott.moore@moretonbay.qld.gov.au>
2016-05-11 11:30:01 +1000

    fixed some issue with forms

--- js/script.js
+++ js/script.js
@@ -2,7 +2,16 @@ var login = document.getElementById('user');
 var overlay = document.getElementsByClassName('overlay')[0];
 var closeBtn = document.getElementsByClassName('btn-close')[0];
 
+var width = window.innerWidth;
+var height = window.innerHeight;
+
 login.addEventListener("click", function() {
+  // overlay.style.left = (width - overlay.offsetLeft) + window.scrollLeft + "px";
+  //overlay.style.top = (height - overlay.offsetTop) + window.scrollTop + "px";
+
+  overlay.offsetLeft = (width - overlay.offsetWidth /2) + document.body.scrollLeft + "px";
+  overlay.offsetTop = (height - overlay.offsetHeight /2) + document.body.scrollTop + "px";
+
   overlay.style.display = "flex";
 }, false);
 
@@ -11,5 +20,5 @@ closeBtn.addEventListener("click", function() {
 }, false);
 
 // overlay.addEventListener("click", function() {
-//   this.style.display = "none";
+//   overlay.style.display = "none";
 // }, false);
