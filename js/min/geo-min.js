b002d211ddd1d6827a656602e1bd0b6dffaefbe1
Elliott Moore <elliott.moore@moretonbay.qld.gov.au>
2016-05-16 11:09:37 +1000

    added basic geolocation code on checkbox - needs 10km search buffer

---
+++ js/min/geo-min.js
@@ -0,0 +1,16 @@
+var includeSuburbs = document.querySelector('#surroundSuburbs');
+
+function showNearbyResults() {
+  if (!includeSuburbs.checked) {
+    return; //if not checked return nothing
+  }
+  navigator.geolocation.getCurrentPosition(function(pos){
+    console.log(pos);
+    //set search buffer radius in here
+    return pos; //else return the coords
+  });
+}
+
+includeSuburbs.addEventListener("change", showNearbyResults);
+
+
