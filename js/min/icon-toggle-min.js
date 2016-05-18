c587b597e1ce2c0cfd6ef4691795c715d4ba8c2d
Elliott Moore <elliott.moore@moretonbay.qld.gov.au>
2016-05-18 16:35:45 +1000

    removed search and added improved search page amoung other things

---
+++ js/min/icon-toggle-min.js
@@ -0,0 +1,28 @@
+var searchBox = document.getElementById('searchBox');
+// var clearBtn = document.getElementById('clearBtn');
+
+//
+// searchField.addEventListener('keyup', function() {
+//   clearBtn.style.visibility = "visible";
+// }, false);
+//
+// clearBtn.addEventListener('click', function() {
+//   searchField.innerHTML = "text";
+// }, false);
+
+(function() {
+  var clearBtn = document.getElementById('clearBtn');
+  
+  searchBox.onkeyup = function() {
+    // Show clear button if text value not empty
+    clearBtn.style.visibility = (this.value.length) ? "visible" : "hidden";
+  };
+
+  // Hide the clear button on click, and reset the input value
+  clearBtn.onclick = function() {
+    this.style.visibility = "hidden";
+    searchBox.value = "";
+  };
+})();
+
+
--- map_viewer.md
+++
@@ -1,5 +0,0 @@
-userName: cab230_test
-lastName: map_viewer
-email   : elliott.moore@connect.qut.edu.au
-passwd  : cab2302016
-https://mbrc.maps.arcgis.com/home/item.html?id=1d07ad771b42477a80120fd70c0f49e4
