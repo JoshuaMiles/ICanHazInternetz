e888c047051abb7902a24ed68c43d4a9431d7746
Elliott Moore <elliott.moore@moretonbay.qld.gov.au>
2016-05-18 17:34:24 +1000

    added DS_Store to gitignore

--- js/icon-toggle.js
+++ js/icon-toggle.js
@@ -1,26 +1,14 @@
 var searchBox = document.getElementById('searchBox');
-// var clearBtn = document.getElementById('clearBtn');
+var clearBtn = document.getElementById('clearBtn');
 
-//
-// searchField.addEventListener('keyup', function() {
-//   clearBtn.style.visibility = "visible";
-// }, false);
-//
-// clearBtn.addEventListener('click', function() {
-//   searchField.innerHTML = "text";
-// }, false);
 
-(function() {
-  var clearBtn = document.getElementById('clearBtn');
-  
-  searchBox.onkeyup = function() {
-    // Show clear button if text value not empty
-    clearBtn.style.visibility = (this.value.length) ? "visible" : "hidden";
-  };
+searchBox.addEventListener('keyup', function() {
+  // Show clear button if input is not empty
+  clearBtn.style.visibility = (this.value.length) ? "visible" : "hidden";
+}, false);
 
-  // Hide the clear button on click, and reset the input value
-  clearBtn.onclick = function() {
-    this.style.visibility = "hidden";
-    searchBox.value = "";
-  };
-})();
+clearBtn.addEventListener('click', function() {
+  // Clear text in input field
+  this.style.visibility = "hidden";
+  searchBox.value = "";
+}, false);
