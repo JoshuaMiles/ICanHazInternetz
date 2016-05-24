1708ab6f594b1c88acb3ff02816cf41ae80d0776
Joshua Miles <joshm@fuckoffjeremy.qut.edu.au>
2016-05-24 11:08:49 +1000

    Registration, SQL and the somethingRandom file which has the updated login system with google streetview

--- somethingRandom.php
+++ somethingRandom.php
@@ -21,7 +21,8 @@
     <div class="navbar">
         <div class="brand"><a href="index.html">routr</a></div>
         <ul>
-            <li id="active"><a href="#" id="user"><i class="material-icons user-icon">person</i><strong>Elliott</strong></a></li> <!--userProfile.html -->
+            <li id="active"><a href="#" id="user"><i class="material-icons user-icon">person</i><strong>
+                        Elliott</strong></a></li> <!--userProfile.html -->
             <li><a href="registration.html">Register</a></li>
             <li><a href="search.html">Find Wifi</a></li>
         </ul>
@@ -49,6 +50,7 @@
                 <a href="registration.html" class="login-notify muted">Don't have an account? Click here</a>
                 <input type="submit" name="login" value="Sign in" id="login">
             </form>
+
         </div>
     </div> <!-- login overlay -->
 
@@ -68,7 +70,9 @@
             <svg class="arc-svg" viewBox="0 0 1440 69" version="1.1" xmlns="http://www.w3.org/2000/svg"
                  xmlns:xlink="http://www.w3.org/1999/xlink">
                 <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                    <path d="M1440,69.0315425 L1440,0 L0,0 L0,69.0315425 C233.264983,25.0227325 473.943868,2 720,2 C966.056132,2 1206.73502,25.0227325 1440,69.0315425 Z" id="Combined-Shape"></path>
+                    <path
+                        d="M1440,69.0315425 L1440,0 L0,0 L0,69.0315425 C233.264983,25.0227325 473.943868,2 720,2 C966.056132,2 1206.73502,25.0227325 1440,69.0315425 Z"
+                        id="Combined-Shape"></path>
                 </g>
             </svg>
             <h2 class="article-head">Recent Places</h2>
@@ -76,25 +80,34 @@
             <?php
             include('server/php/pdo.php');
 
+            include('server/php/databaseQueries.php');
+
+
 
             $pdo = getPDO();
-            $result = $pdo->prepare('SELECT NAME,ADDRESS,SUBURB FROM hotspots.wifihotspots LIMIT 9;');
+            $result = $pdo->prepare('SELECT NAME,ADDRESS,SUBURB,LATITUDE,LONGITUDE FROM hotspots.items LIMIT 9;');
             $result->execute();
+
+
 
             echo '<article class="results-container">';
             echo '<div class="container">';
             echo '<div class="review-cards">';
+            $anAverage = new Database(getPDO());
+            $anAverage->getAverageForRating('7th Brigade Park');
             foreach ($result as $hotspot) {
 
                 echo '<a href="sampleItem.html" class="review-card">';
                 echo '<div class="media">';
-                echo '<img src="images/img-test.png" alt="hotspot-img">';
+                echo '<img src="https://maps.googleapis.com/maps/api/streetview?maptype=satellite&heading=151.78&pitch=-0.76&location=' . $hotspot['LATITUDE'] . ',' . $hotspot['LONGITUDE'] . '&zoom=18&size=300x200" alt="hotspot-img">';
                 echo '</div>';
                 echo '<div class="desc">';
                 // Change to database values from sql query
-                echo '<div class"title">' . $hotspot['NAME'] . '</div>';
+//                echo '<div class"title">' . $hotspot['NAME'] . '</div>';
 //                echo '<div class"muted">' . $hotspot['RATING'] . '</div>';
-                echo '<div class"muted">' . $hotspot['ADDRESS'] . $hotspot['SUBURB'] .  '</div>';
+//                echo '<div class"title">' .  . '</div>';
+//                echo '<div class"muted">' . $hotspot['ADDRESS'] . $hotspot['SUBURB'] .  '</div>';
+                echo '<div class"muted">' . $hotspot['SUBURB'] .  '</div>';
                 echo '</div>';
                 echo '</a>';
 
