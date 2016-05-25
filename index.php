a0e058ddc019d5c9eb29ae579177a009660272bb
Elliott Moore <elliott.moore@moretonbay.qld.gov.au>
2016-05-25 17:32:32 +1000

    added template file to databaseQueries

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php include_once('server/PHP/databaseQueries.php'); ?>
 <!DOCTYPE html>
 <html>
   <head>
--- server/PHP/databaseQueries.php
+++ server/PHP/databaseQueries.php
@@ -20,31 +20,35 @@ class Database {
     $qry = $this->db->prepare('SELECT NAME,ADDRESS,SUBURB,LATITUDE,LONGITUDE FROM hotspots.items LIMIT 9;'); // :name , address = :address, suburb = :suburb, rating = :rating');
     $qry->execute();
 
-    echo '<article class="results-container">';
-    echo '<div class="container">';
-    echo '<div class="review-cards">';
+    // echo '<article class="results-container">';
+    // echo '<div class="container">';
+    // echo '<div class="review-cards">';
 
 
     foreach ($qry as $hotspot) {
 
-      echo '<a itemscope itemtype="http://schema.org/GeoCoordinates" href="sampleItem.html" class="review-card">';
-      echo '<div class="media">';
-      echo '<input type ="hidden" itemprop="latitude" content="' . $hotspot['LATITUDE'] . '">';
-      echo '<input type ="hidden" itemprop="longitude" content="' . $hotspot['LONGITUDE'] . '">';
-      echo '<img  src="https://maps.googleapis.com/maps/api/streetview?maptype=satellite&heading=151.78&pitch=-0.76&location=' . $hotspot['LATITUDE'] . ',' . $hotspot['LONGITUDE'] . '&zoom=18&size=300x200" alt="hotspot-img">';
-      echo '</div>';
-      echo '<div itemscope itemtype="http://schema.org/Place" class="desc">';
-      // Change to database values from sql query
-      echo '<div class"title">' . $hotspot['NAME'] . '</div>';
-//                echo '<div itemprop="AggregateRating" class"muted">' . $hotspot['RATING'] . '</div>';
+      include('server/includes/recentReview.tpl.php');
 
-      echo '<div itemprop="address" class"muted">' . $hotspot['ADDRESS'] . ',' . $hotspot['SUBURB'] . '</div>';
-//                echo '<div class"muted">' . $hotspot['SUBURB'] .  '</div>';
-      echo '</div>';
-      echo '</a>';
     }
-    echo '</div>';
-    echo '</article>';
+//
+//       echo '<a itemscope itemtype="http://schema.org/GeoCoordinates" href="sampleItem.html" class="review-card">';
+//       echo '<div class="media">';
+//       echo '<input type ="hidden" itemprop="latitude" content="' . $hotspot['LATITUDE'] . '">';
+//       echo '<input type ="hidden" itemprop="longitude" content="' . $hotspot['LONGITUDE'] . '">';
+//       echo '<img  src="https://maps.googleapis.com/maps/api/streetview?maptype=satellite&heading=151.78&pitch=-0.76&location=' . $hotspot['LATITUDE'] . ',' . $hotspot['LONGITUDE'] . '&zoom=18&size=300x200" alt="hotspot-img">';
+//       echo '</div>';
+//       echo '<div itemscope itemtype="http://schema.org/Place" class="desc">';
+//       // Change to database values from sql query
+//       echo '<div class"title">' . $hotspot['NAME'] . '</div>';
+// //                echo '<div itemprop="AggregateRating" class"muted">' . $hotspot['RATING'] . '</div>';
+//
+//       echo '<div itemprop="address" class"muted">' . $hotspot['ADDRESS'] . ',' . $hotspot['SUBURB'] . '</div>';
+// //                echo '<div class"muted">' . $hotspot['SUBURB'] .  '</div>';
+//       echo '</div>';
+//       echo '</a>';
+//     }
+//     echo '</div>';
+//     echo '</article>';
 
   }
 
@@ -123,5 +127,3 @@ class Database {
 }
 
 ?>
-
-
