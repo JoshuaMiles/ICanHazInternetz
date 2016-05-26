16089a9e3c0b9f98612ca3d3afb61aad0b6e1ef3
Elliott Moore <elliott.moore@moretonbay.qld.gov.au>
2016-05-26 17:16:34 +1000

    gfdgfd

--- server/PHP/login.php
+++ server/PHP/login.php
@@ -3,7 +3,7 @@
   include("postMaster.php");
   include("user.php");
 
-  $errors = array();
+  // $errors = array();
 
   if ($_SERVER['REQUEST_METHOD'] == "POST") {
     $email = getPost('email');
@@ -15,11 +15,11 @@
     // Call login function
     $login->login($email, $password);
 
-    if (!isset($email)) {
-      $error[] = 'please enter email address';
-    }
-    if (!isset($password)) {
-      $error[] = 'please enter password';
-    }
+    // if (!isset($email)) {
+    //   $error[] = 'please enter email address';
+    // }
+    // if (!isset($password)) {
+    //   $error[] = 'please enter password';
+    // }
   }
 ?>
--- server/PHP/user.php
+++ server/PHP/user.php
@@ -50,6 +50,7 @@ class User {
    */
   public function login($postEmail, $postPassword) {
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+      $errors = array();
 
       $statement = $this->db->prepare('SELECT email,firstName, password_hash FROM hotspots.members WHERE email = ?');
       $statement->bindParam(1, $postEmail);
@@ -67,6 +68,8 @@ class User {
           header("Location:  http://{$_SERVER['HTTP_HOST']}/index.php");
           exit();
         } else {
+          $error[] = 'please enter email address';
+          $error[] = 'please enter password';
         }
       } else {
         echo "INVALID EMAIL";
