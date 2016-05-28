408f7e79d07c43815dafd00beb649049f393698e
Elliott Moore <elliott.moore@moretonbay.qld.gov.au>
2016-05-29 00:15:13 +1000

    fixed rating dropdown

--- server/includes/searchRating.tpl.php
+++ server/includes/searchRating.tpl.php
@@ -1,12 +1,12 @@
 <?php
   echo('
     <select name="search-rating" class="rating-select-box">
-      <option disabled selected value="">Rating...</option>
-      <option disabled selected value="1">1</option>
-      <option disabled selected value="2">2</option>
-      <option disabled selected value="3">3</option>
-      <option disabled selected value="4">4</option>
-      <option disabled selected value="5">5</option>
+      <option value="">Rating...</option>
+      <option value="1">1</option>
+      <option value="2">2</option>
+      <option value="3">3</option>
+      <option value="4">4</option>
+      <option value="5">5</option>
     </select>
   ');
 ?>
