23f474f6aaa1240240b6c2314f093dd9422367a0
Elliott Moore <elliott.moore@moretonbay.qld.gov.au>
2016-05-28 21:10:55 +1000

    more review styles

--- server/includes/addReview.tpl.php
+++ server/includes/addReview.tpl.php
@@ -8,7 +8,7 @@
 
       <form class="addReviewForm" action="sampleItem.php" method="post">
         <textarea name="userComment" class="muted" id="userComment" rows="5" placeholder="Your rating helps others find better Wifi."></textarea>
-          <p>Rating:</p>
+          <p id="rating-title">Rating:</p>
           <input type="radio" name="rating-value" id="" class="addRating" value="1">1
           <input type="radio" name="rating-value" id="" class="addRating" value="2">2
           <input type="radio" name="rating-value" id="" class="addRating" value="3">3
