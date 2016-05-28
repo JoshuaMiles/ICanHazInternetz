9c71724e27e33709782caf003adcc08ec6aabdd1
Elliott Moore <elliott.moore@moretonbay.qld.gov.au>
2016-05-28 13:14:53 +1000

    added inline styles to map div

--- server/includes/initialState.tpl.php
+++ server/includes/initialState.tpl.php
@@ -1,6 +1,6 @@
 <div class="search-instructions">
   <span id="search-headline">Search for a hotspot above.</span>
   <div class="map all-records">
-    <div id="map"></div>
+    <div id="map" style="width: 1000px; height: 500px; margin: 0 auto;"></div>
   </div>
 </div>
