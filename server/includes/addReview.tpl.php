d8dd41f17801c05a755e2487e56ca68aa6ef99bf
Elliott Moore <elliott.moore@moretonbay.qld.gov.au>
2016-05-28 21:04:29 +1000

    added labels to radio buttons on addReview.tpl

--- server/includes/addReview.tpl.php
+++ server/includes/addReview.tpl.php
@@ -8,21 +8,21 @@
 
       <form class="addReviewForm" action="sampleItem.php" method="post">
         <textarea name="userComment" class="muted" id="userComment" rows="5" placeholder="Your rating helps others find better Wifi."></textarea>
-          <input type="radio" name="rating-value" id="" class="addRating" value="1">
-          <input type="radio" name="rating-value" id="" class="addRating" value="2">
-          <input type="radio" name="rating-value" id="" class="addRating" value="3">
-          <input type="radio" name="rating-value" id="" class="addRating" value="4">
-          <input type="radio" name="rating-value" id="" class="addRating" value="5">
-    
-        <?php
-          if(!isset($_SESSION['logged_in'])) {
-            echo('<a href="#" id="user" class="login-notify">Login to add a review</a>');
-          } else if(isset($_SESSION['logged_in'])){
-            echo('<button id="submitReview">Submit comment</button>');
-          }
+          <p>Rating:</p>
+          <input type="radio" name="rating-value" id="" class="addRating" value="1">1
+          <input type="radio" name="rating-value" id="" class="addRating" value="2">2
+          <input type="radio" name="rating-value" id="" class="addRating" value="3">3
+          <input type="radio" name="rating-value" id="" class="addRating" value="4">4
+          <input type="radio" name="rating-value" id="" class="addRating" value="5">5
+          <?php
+            if(!isset($_SESSION['logged_in'])) {
+              echo('<a href="#" id="user" class="login-notify">Login to add a review</a>');
+            } else if(isset($_SESSION['logged_in'])){
+              echo('<button id="submitReview">Submit comment</button>');
+            }
 
-        ?>
-      <div class="clearfix"></div>
+          ?>
+        <div class="clearfix"></div>
       </form>
     </div>
   </div>
