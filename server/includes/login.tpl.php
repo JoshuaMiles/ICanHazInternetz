17cb982e1a06e0dca5fff06d85f197abdbefaf04
Elliott Moore <elliott.moore@moretonbay.qld.gov.au>
2016-05-28 13:29:48 +1000

    fixed login page

--- server/includes/login.tpl.php
+++ server/includes/login.tpl.php
@@ -1,19 +1,17 @@
-<div class="overlay">
-  <div class="modal">
-    <h2 class="article-head">Login</h2>
-    <hr class="article-title-rule">
+<div class="modal">
+  <h2 class="article-head">Login</h2>
+  <hr class="article-title-rule">
 
-    <form method="POST" action="" class="login-form">
-      <div class="input-group">
-        <input type="email" name="email" id="username" class="lbl-highlight">
-        <label for="firstName">Email</label>
-      </div>
-      <div class="input-group">
-        <input type="password" name="password" id="password" class="lbl-highlight">
-        <label for="password">Password</label>
-      </div>
-      <a href="registration.php" class="login-notify muted">Don't have an account? Click here</a>
-      <input type="submit" name="login" value="Sign in" id="login">
-    </form>
-  </div>
+  <form method="POST" action="" class="login-form">
+    <div class="input-group">
+      <input type="email" name="email" id="username" class="lbl-highlight">
+      <label for="firstName">Email</label>
+    </div>
+    <div class="input-group">
+      <input type="password" name="password" id="password" class="lbl-highlight">
+      <label for="password">Password</label>
+    </div>
+    <a href="registration.php" class="login-notify muted">Don't have an account? Click here</a>
+    <input type="submit" name="login" value="Sign in" id="login">
+  </form>
 </div>
