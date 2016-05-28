0ddf4e62292c5862e41bc3ba932d3e80c2a1936a
Joshua Miles <josh@fuckoffjeremy.com>
2016-05-28 13:04:04 +1000

    fuck

--- js/map.js
+++ js/map.js
@@ -1,56 +1,56 @@
+function load() {
+  var map = new google.maps.Map(document.getElementById("map"), {
+    center: new google.maps.LatLng(-27.4710, 153.0234),
+    zoom: 13,
+    mapTypeId: 'roadmap'
+  });
+  var infoWindow = new google.maps.InfoWindow;
 
-    function load() {
-      var map = new google.maps.Map(document.getElementById("map"), {
-        center: new google.maps.LatLng(-27.4710, 153.0234),
-        zoom: 13,
-        mapTypeId: 'roadmap'
+  // Change this depending on the name of your PHP file
+  downloadUrl("server/PHP/createMarkers.php", function (data) {
+    var html;
+    var xml = data.responseXML;
+    var markers = xml.documentElement.getElementsByTagName("marker");
+    for (var i = 0; i < markers.length; i++) {
+      var name = markers[i].getAttribute("name");
+      var address = markers[i].getAttribute("address");
+      var point = new google.maps.LatLng(
+        parseFloat(markers[i].getAttribute("lat")),
+        parseFloat(markers[i].getAttribute("long")));
+      html = "<b>" + name + "</b> <br/>" + address + "<a href='sampleItem.php?hotspotName='  name  > More info </a>"
+
+      ;
+      var marker = new google.maps.Marker({
+        map: map,
+        position: point
       });
-      var infoWindow = new google.maps.InfoWindow;
-
-      // Change this depending on the name of your PHP file
-      downloadUrl("server/PHP/createMarkers.php", function(data) {
-        var xml = data.responseXML;
-        var markers = xml.documentElement.getElementsByTagName("marker");
-        for (var i = 0; i < markers.length; i++) {
-          var name = markers[i].getAttribute("name");
-          var address = markers[i].getAttribute("address");
-          var point = new google.maps.LatLng(
-              parseFloat(markers[i].getAttribute("lat")),
-              parseFloat(markers[i].getAttribute("long")));
-          var html = {
-            "<b>" + name + "</b> <br/>" + address +
-            "<a href='sampleItem.php?hotspotName='" + name + ">";
-          };
-          var marker = new google.maps.Marker({
-            map: map,
-            position: point
-          });
-          bindInfoWindow(marker, map, infoWindow, html);
-        }
-      });
+      bindInfoWindow(marker, map, infoWindow, html);
     }
+  });
+}
 
-    function bindInfoWindow(marker, map, infoWindow, html) {
-      google.maps.event.addListener(marker, 'click', function() {
-        infoWindow.setContent(html);
-        infoWindow.open(map, marker);
-      });
+function bindInfoWindow(marker, map, infoWindow, html) {
+  google.maps.event.addListener(marker, 'click', function () {
+    infoWindow.setContent(html);
+    infoWindow.open(map, marker);
+  });
+}
+
+function downloadUrl(url, callback) {
+  var request = window.ActiveXObject ?
+    new ActiveXObject('Microsoft.XMLHTTP') :
+    new XMLHttpRequest;
+
+  request.onreadystatechange = function () {
+    if (request.readyState == 4) {
+      request.onreadystatechange = doNothing;
+      callback(request, request.status);
     }
+  };
 
-    function downloadUrl(url, callback) {
-      var request = window.ActiveXObject ?
-          new ActiveXObject('Microsoft.XMLHTTP') :
-          new XMLHttpRequest;
+  request.open('GET', url, true);
+  request.send(null);
+}
 
-      request.onreadystatechange = function() {
-        if (request.readyState == 4) {
-          request.onreadystatechange = doNothing;
-          callback(request, request.status);
-        }
-      };
-
-      request.open('GET', url, true);
-      request.send(null);
-    }
-
-    function doNothing() {}
+function doNothing() {
+}
